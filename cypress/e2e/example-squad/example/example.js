9bd2b633d20e8a61e0c2cbef00ffdc3a941a6be1
Edo Krishnanda Aditya <edo.aditya@bfi.co.id>
2023-11-01 16:17:34 +0800

    setup linter and prettier

--- cypress/e2e/example-squad/example/example.js
+++ cypress/e2e/example-squad/example/example.js
@@ -1,13 +1,10 @@
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import { When, Then } from '@badeball/cypress-cucumber-preprocessor';
 import ExamplePage from '../../../pom/example-squad/example/examplePage';
 
-
-
-
-When("I visit duckduckgo.com", () => {
-  cy.visit("https://duckduckgo.com/");
+When('I visit duckduckgo.com', () => {
+  cy.visit('https://duckduckgo.com/');
 });
 
-Then("I should see a search bar", () => {
-  ExamplePage.searchBar().should('be.visible')
+Then('I should see a search bar', () => {
+  ExamplePage.searchBar().should('be.visible');
 });
